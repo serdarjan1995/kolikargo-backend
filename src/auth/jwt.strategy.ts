5741f6b1d89083f87e6a87f244cdc6cfe7490320
Sardor <serdarjan1995@gmail.com>
2022-08-10 01:38:13 +0300

    JWT check users in DB while validating tokens

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,10 +3,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
 import { AuthenticatedUser } from '../user/models/user.model';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -15,10 +16,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any): Promise<AuthenticatedUser> {
-    return {
+    const userData = {
       phoneNumber: payload.phoneNumber,
       userId: payload.userId,
       roles: payload.roles,
     };
+    await this.userService.getUserBy(userData, true, false);
+    return userData;
   }
 }
